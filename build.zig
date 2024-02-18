7a22ebf3da31f4ceb85dd994a3799ee1e8399c3e
radar roark <122068506+radarroark@users.noreply.github.com>
2024-02-18 09:25:47 -0500

    remove addStaticLibrary

--- build.zig
+++ build.zig
@@ -2,17 +2,7 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-
     const optimize = b.standardOptimizeOption(.{});
-
-    const lib = b.addStaticLibrary(.{
-        .name = "xitdb",
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-
-    b.installArtifact(lib);
 
     const unit_tests = b.addTest(.{
         .root_source_file = .{ .path = "src/test.zig" },
